2029c27b7a686267359428f269013eda990218ba
Dave Powers <djpowers89@gmail.com>
2014-01-06 17:57:08 -0500

    Add sign out feature, change root to pages#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spotcheck::Application.routes.draw do
   devise_for :users
-  root 'projects#index'
+  root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/features/user_signs_out_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+feature 'user signs out', %Q{
+  As a user
+  I want to sign out
+  So that my account is not accessible
+} do
+
+  # Acceptance Criteria
+  # While signed in, I can sign out to end my user session
+
+  scenario 'authenticated user signs out' do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+
+    expect(page).to have_content('Sign Out')
+    click_link 'Sign Out'
+
+    expect(page).to have_content('Signed out successfully.')
+  end
+
+end
