0ef812cf8bb8097f4b85a7d3c0826b27b48b0d0f
Dave Powers <djpowers89@gmail.com>
2014-01-07 14:49:09 -0500

    Implement add project. Add more flexible flash behavior in application.html.erb

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,32 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @projects = Project.all #user only
+    @projects = Project.all.where(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+
+    if @project.save
+      flash[:notice] = 'Project was successfully created.'
+      redirect_to @project
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+    def project_params
+      params.require(:project).permit(:title, :description, :status, :due_date)
+    end
 
 end
---
+++ spec/features/user_creates_project_spec.rb
@@ -0,0 +1,76 @@
+require 'spec_helper'
+
+feature 'user creates project', %Q{
+  As an authenticated user
+  I want to create a project
+  So that my related videos can be kept and viewed together
+} do
+
+  # Acceptance Criteria
+  # user must be signed in to add project
+  # project name must be specified
+  # project name cannot be duplicated
+  # creating a project shows an empty project window
+  # empty project name triggers error message
+
+  def user_sign_in
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Sign In'
+  end
+
+  scenario 'user is not authenticated' do
+    visit new_project_path
+
+    expect(page).to have_content('You need to sign in or sign up before continuing.')
+  end
+
+  scenario 'required information is supplied' do
+    user_sign_in
+    click_link 'New Project'
+
+    project = FactoryGirl.build(:project)
+
+    fill_in 'Title', with: project.title
+    fill_in 'Description', with: project.description
+    fill_in 'Status', with: project.status
+
+    within '#project_due_date_1i' do
+      select '2014'
+    end
+    within '#project_due_date_2i' do
+      select 'February'
+    end
+    within '#project_due_date_3i' do
+      select '28'
+    end
+    within '#project_due_date_4i' do
+      select '17'
+    end
+    within '#project_due_date_5i' do
+      select '00'
+    end
+
+    click_button 'Create Project'
+
+    expect(page).to have_content('Project was successfully created.')
+    expect(page).to have_content(project.title)
+    expect(page).to have_content(project.description)
+    expect(page).to have_content(project.status)
+    expect(page).to have_content("2014-02-28 17:00")
+  end
+
+    scenario 'required information is not supplied' do
+      user_sign_in
+      visit new_project_path
+      click_button 'Create Project'
+
+      within '.input.project_title' do
+        expect(page).to have_content("can't be blank")
+      end
+    end
+
+end
